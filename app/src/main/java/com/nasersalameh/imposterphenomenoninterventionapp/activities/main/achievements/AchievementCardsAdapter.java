b46ce56961ac92d206f6b11ab7d92405ca88a8f4
NaserSalameh <NaserSalameh97@gmail.com>
2021-03-01 16:18:41 +0000

    Sprint 3: Achievement + Abilities Tab Last Changes: - Created Achievement Cards and Adapter - Added Cards To UI Sprint progress: NA Sprint TO DO: - Create Achievements Functionality:   - Add Images to Achievements based on Type   - Add Functionality to open Achievement   - Add Functionality to add new Achievements - Create Abilities UI and Functionality

---
+++ app/src/main/java/com/nasersalameh/imposterphenomenoninterventionapp/activities/main/achievements/AchievementCardsAdapter.java
@@ -0,0 +1,107 @@
+package com.nasersalameh.imposterphenomenoninterventionapp.activities.main.achievements;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Parcelable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.nasersalameh.imposterphenomenoninterventionapp.R;
+import com.nasersalameh.imposterphenomenoninterventionapp.models.Achievement;
+import com.nasersalameh.imposterphenomenoninterventionapp.models.Information;
+
+import java.util.ArrayList;
+
+public class AchievementCardsAdapter extends RecyclerView.Adapter<AchievementCardsAdapter.ViewHolder> {
+
+    public static final int INFORMATION_ACTIVITY_RESULT = 101;
+    private ViewHolder cardView;
+
+    private LayoutInflater layoutInflater;
+    private ArrayList<Achievement> achievementList;
+    private int achievementIndex;
+
+    private Activity mainActivity;
+
+    public AchievementCardsAdapter(Context context, ArrayList<Achievement> achievementList, Activity mainActivity){
+        this.layoutInflater = LayoutInflater.from(context);
+        this.achievementList = achievementList;
+
+        this.mainActivity = mainActivity;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View view = layoutInflater.inflate(R.layout.fragment_achievements_card,viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int index) {
+
+        //Bind card view
+        cardView = viewHolder;
+
+        //bind Card with Behaviour
+        this.achievementIndex = index;
+//        viewHolder.achievementImageView.setText(achievementList.get(index).getInformationName());
+        viewHolder.achievementTextView.setText(achievementList.get(index).getAchievementName());
+
+//        //Set on Click Listener to View Card
+//        viewHolder.cardView.setOnClickListener(v -> {
+//
+//            //Start Information Activity
+//            Intent startInformationCardActivity = new Intent(mainActivity, InformationCardActivity.class);
+//
+//            //Add current information to intent
+//            startInformationCardActivity.putExtra("Information", informationList.get(index));
+//
+//            mainActivity.startActivity(startInformationCardActivity);
+//        });
+
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return super.getItemId(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return achievementList.size();
+    }
+
+    //Tie the UI to the adapter
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        MaterialCardView cardView;
+
+        ImageView achievementImageView;
+
+        TextView achievementTextView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            cardView = itemView.findViewById(R.id.achievementCardView);
+
+            achievementImageView = itemView.findViewById(R.id.achievementImageView);
+            achievementTextView = itemView.findViewById(R.id.achievementTextView);
+
+        }
+    }
+
+
+
+}
--- app/src/main/java/com/nasersalameh/imposterphenomenoninterventionapp/activities/main/achievements/AchievementsFragment.java
+++ app/src/main/java/com/nasersalameh/imposterphenomenoninterventionapp/activities/main/achievements/AchievementsFragment.java
@@ -1,5 +1,7 @@
 package com.nasersalameh.imposterphenomenoninterventionapp.activities.main.achievements;
 
+import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,28 +10,70 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.nasersalameh.imposterphenomenoninterventionapp.R;
+import com.nasersalameh.imposterphenomenoninterventionapp.activities.main.information.InformationCardsAdapter;
+import com.nasersalameh.imposterphenomenoninterventionapp.database.AchievementData;
+import com.nasersalameh.imposterphenomenoninterventionapp.database.DatabaseHelper;
+import com.nasersalameh.imposterphenomenoninterventionapp.database.InformationData;
+import com.nasersalameh.imposterphenomenoninterventionapp.models.Achievement;
+import com.nasersalameh.imposterphenomenoninterventionapp.models.AchievementType;
+import com.nasersalameh.imposterphenomenoninterventionapp.models.Information;
+
+import java.util.ArrayList;
 
 public class AchievementsFragment extends Fragment {
 
     private AchievementsViewModel achievementsViewModel;
 
+    private Activity mainActivity;
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         achievementsViewModel =
                 new ViewModelProvider(this).get(AchievementsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_achievements, container, false);
-        final TextView textView = root.findViewById(R.id.text_achievement);
-        achievementsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        mainActivity = getActivity();
+        setUpRecyclerView(root);
         return root;
     }
+
+    private ArrayList<Achievement> loadAchievementFromDatabase() {
+        //get Achievement from Usage Database
+        DatabaseHelper databaseHelper = new DatabaseHelper(mainActivity);
+        AchievementData achievementData = new AchievementData(databaseHelper);
+
+        ArrayList<Achievement> testArray = new ArrayList<>();
+        AchievementType testType = new AchievementType("testType");
+        for(int i=0;i<5;i++) {
+            testArray.add(new Achievement("TEST"+i, testType, System.currentTimeMillis()));
+
+        }
+        return testArray;
+//        return achievementData.getAchievementList();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void setUpRecyclerView(View root){
+        //Get (updated) information List
+        ArrayList<Achievement> achievementList = loadAchievementFromDatabase();
+
+        //get Recycler View:
+        RecyclerView achievementRecyclerView = root.findViewById(R.id.achievementsRecyclerView);
+        achievementRecyclerView.setLayoutManager(new GridLayoutManager(mainActivity, 3));
+
+        //Set up recycler adapter with information from usage database
+        AchievementCardsAdapter adapter = new AchievementCardsAdapter(mainActivity, achievementList,mainActivity);
+        achievementRecyclerView.setAdapter(adapter);
+    }
+
 }
