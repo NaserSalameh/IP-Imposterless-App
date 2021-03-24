b4f7d73d2130f0706554422147031b63b8a5833a
NaserSalameh <NaserSalameh97@gmail.com>
2021-03-24 00:31:50 +0000

    Sprint 5: Content + Visual + Fixes Last Changes: - Added background and text color variable Sprint progress: - Added Back Buttons - Added Logging - Added Content to DB - Added Content across App Sprint TO DO: - Fix BG Color and Font Color Visuals - Fix Popup Visuals!

--- app/src/main/java/com/nasersalameh/imposterphenomenoninterventionapp/models/Ability.java
+++ app/src/main/java/com/nasersalameh/imposterphenomenoninterventionapp/models/Ability.java
@@ -69,31 +69,4 @@ public class Ability implements Serializable {
         this.experience = experience;
     }
 
-    public int getLevelColor(){
-        int level = getLevel();
-        switch (level){
-            case 1: return R.color.level_1;
-            case 2: return R.color.level_2;
-            case 3: return R.color.level_3;
-            case 4: return R.color.level_4;
-            case 5: return  R.color.level_5;
-            case 6: return  R.color.level_6;
-            case 7: return  R.color.level_7;
-            case 8: return  R.color.level_8;
-            case 9: return  R.color.level_9;
-            case 10: return R.color.level_10;
-            case 11: return R.color.level_11;
-            case 12: return R.color.level_12;
-            case 13: return R.color.level_13;
-            case 14: return R.color.level_14;
-            case 15: return R.color.level_15;
-            case 16: return R.color.level_16;
-            case 17: return R.color.level_17;
-            case 18: return R.color.level_18;
-            case 19: return R.color.level_19;
-            case 20: return R.color.level_20;
-        }
-        return Color.RED;
-    }
-
 }
